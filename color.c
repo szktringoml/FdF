b1f352345d75f2db6e1982fb2acc1f4ce6b4fa38
Kouta Suzuki <kousuzuk@c1r8s1.42tokyo.jp>
2023-10-09 20:22:23 +0900

    dxの中のxにおけるcurr_zを出力する関数がほしいよねってことでbresenhamを利用しようとしている

--- color.c
+++ color.c
@@ -6,7 +6,7 @@
 /*   By: kousuzuk <kousuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 18:17:03 by kousuzuk          #+#    #+#             */
-/*   Updated: 2023/10/09 20:03:37 by kousuzuk         ###   ########.fr       */
+/*   Updated: 2023/10/09 20:21:12 by kousuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,46 @@ void get_min_max_z(t_fdf *fdf_info, int *minz, int *maxz)
         *maxz = fdf_info->coordinate->z_start;
     }
 }
-void	get_color_by_z_value(t_fdf *fdf_info, int dx, int x)
+
+void	bresenham_color(t_fdf *fdf_info, int dx, int d)
 {
-    int z;
+	int	e;
+	int	y;
+	int	color;
+    d = 2 * (maxz - minz) * x;
+
+	color = 0;
+	e = 0;
+	y = 0;
+	while (color < d)
+	{
+		e = e + d;
+		if (e > dx)
+		{
+			y = y + 1;
+			e = e - (2 * dx);
+		}
+		color++;
+	}
+}
+
+int get_z(t_fdf *fdf_info, int *z, int dx, int x)
+{
     if(fdf_info->coordinate->z_start == fdf_info->coordinate->z_finish)
-        z = fdf_info->coordinate->z_start;
+        *z = fdf_info->coordinate->z_start;
 	else   
     {
         int minz;
         int maxz;
         get_min_max_z(fdf_info, &minz, &maxz);
-
         
-        z = minz + ((maxz-maxz/dx) * x);
-
-
-
+        
+        *z = minz + ((maxz-maxz/dx) * x);
     }
+}
+
+void	get_color_by_z_value(t_fdf *fdf_info, int dx, int x)
+{
+    int z;
+    z = get_z(fdf_info, &z, dx, x);
 }
