51cc9c9c039e9bfaa9d9c33264cbe14781661dc4
Kouta Suzuki <kousuzuk@c1r10s1.42tokyo.jp>
2023-10-11 18:35:16 +0900

    shift leftできた

--- apply_properties.c
+++ apply_properties.c
@@ -6,7 +6,7 @@
 /*   By: kousuzuk <kousuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:26:09 by kousuzuk          #+#    #+#             */
-/*   Updated: 2023/10/09 18:12:27 by kousuzuk         ###   ########.fr       */
+/*   Updated: 2023/10/11 18:31:09 by kousuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,14 @@ void	apply_properties_to_one_line_points(t_fdf *fdf_info)
 	[fdf_info->coordinate->x_start];
 	z_finish = fdf_info->z_values[fdf_info->coordinate->y_finish]
 	[fdf_info->coordinate->x_finish];
-	fdf_info->color = (z_start) ? 0xe80c0c : 0xffffff; ///////////////normここだけ(4コつく)
+	printf("%d\n", z_start);
+	if(z_start > 0)
+		fdf_info->color = 0xff2222;
+	else if(z_start < 0)
+		fdf_info->color = 0x7777ff;
+	else
+		fdf_info->color =  0xffffff;
+	// fdf_info->color = (z_start) ? 0xe80c0c : 0xffffff; ///////////////normここだけ(4コつく)
 	apply_zoom_to_one_line_points(fdf_info);
 	apply_degrees(&(fdf_info->coordinate->x_start),
 		&(fdf_info->coordinate->y_start),
